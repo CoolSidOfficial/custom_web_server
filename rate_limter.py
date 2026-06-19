680ca1779e7d2aab1a1bbf6c3dcf05255367b5d5
CoolSid <hackersiddhant1@gmail.com>
2026-06-20 01:37:43 +0530

    adding a rate limtier

---
+++ rate_limter.py
@@ -0,0 +1,35 @@
+mport time
+
+
+class RateLimiter:
+
+        def __init__(self, limit=5, window=60):
+                    self.limit = limit
+                            self.window = window
+                                    self.requests = {}
+
+
+                                        def allow(self, client_ip):
+
+                                                    now = time.time()
+
+                                                            if client_ip not in self.requests:
+                                                                            self.requests[client_ip] = []
+
+
+                                                                                    # remove expired requests
+                                                                                            self.requests[client_ip] = [
+                                                                                                                request_time
+                                                                                                                            for request_time in self.requests[client_ip]
+                                                                                                                                        if now - request_time < self.window
+                                                                                                                                                ]
+
+
+                                                                                                    # limit exceeded
+                                                                                                            if len(self.requests[client_ip]) >= self.limit:
+                                                                                                                            return False
+
+
+                                                                                                                                self.requests[client_ip].append(now)
+
+                                                                                                                                        return True
